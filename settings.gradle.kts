8c604d8ce48f850530f2791a97bcae74d00b7ea6
Driptaroop Das <driptaroop.das@gmail.com>
2020-10-14 19:17:38 +0200

    directory structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,13 @@
 rootProject.name = "clean-spring"
 
 include(
-    "app"
+        "app",
+        "adapters:driven:persistence",
+        "adapters:drivers:web",
+        "core:domain:entity",
+        "core:domain:service",
+        "core:ports:incoming:api",
+        "core:ports:incoming:model",
+        "core:ports:outgoing:model",
+        "core:ports:outgoing:spi"
 )
