9d238e25573a76de0d86b82791e040b11ec0243d
Driptaroop Das <driptaroop.das@gmail.com>
2020-10-15 14:57:08 +0200

    correction

--- adapters/driven/persistence/build.gradle.kts
+++ adapters/driven/persistence/build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     api(project(":common:common-persistence"))
+    api(project(":core:ports:outgoing:spi"))
     runtimeOnly("com.h2database:h2")
 }
